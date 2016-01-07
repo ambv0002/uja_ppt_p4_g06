77218c86cbf00b4f77a0d86c310091ecd72f9ac0
ambv0002 <ambv0002@red.ujaen.es>
2016-01-07 16:28:30 +0100

    comentarios

--- Practica_4/src/ujaen/git/ppt/smtp/RFC5322.java
+++ Practica_4/src/ujaen/git/ppt/smtp/RFC5322.java
@@ -1,4 +1,11 @@
 package ujaen.git.ppt.smtp;
+// Se definen los fineros SMTP.
+// SP, CRLF, ENDMSG.
+// El servidor debe incluir en el correo las cabeceras Message-ID y Received, según la RFC 5322.
+//  Los correos se reciben en archivos txt en carpetas diferentes para cada usuario.
+// Los ficheros deben seguir el formato de la RFC 5322. 
+// Para almacer la clave de usuario, se crea en el directorio de usuario un fichero user.key, 
+// donde user es el ID del usuario. Sólo puede contener una línea con una cabecera sin espacios que señala la clave.
 
 public interface RFC5322 {
 		
